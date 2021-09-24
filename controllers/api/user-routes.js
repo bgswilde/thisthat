2ca24aac0d89daabb3d755ff8ababc682809795c
Terror666 <ckuthulu@gmail.com>
2021-09-24 11:05:12 -0500

    session and cookie logic started

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -50,7 +50,14 @@ router.post('/', (req,res) => {
         username: req.body.username,
         password: req.body.password
     })
-    .then(dbUserData=> res.json(dbUserData))
+    .then(dbUserData=> /*{
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+        })
+    }*/
+    res.json(dbUserData))
     .catch(err=> {
         console.log(err);
         res.status(500).json(err);
--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const { Sequelize } = require('sequelize/types');
+const expresshb = require('express-handlebars');
+const hbs = expresshb.create({});
+const path = require('path');
+
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
+require('dotenv').config();
+const s = {
+    secret: process.env.DB_Secret,
+    cookie: {},
+    resave: false,
+    saveUninitalized: true,
+    store: new sequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(s));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(routes);
 
