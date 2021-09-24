69d48cbcf5a38643f5efc04384461ff45fbfae65
georgedmendoza <georgedmendoza13@gmail.com>
2021-09-24 08:38:57 -0500

    associations for modoles, first trial

--- models/index.js
+++ models/index.js
@@ -3,9 +3,35 @@ const Question = require('./Question');
 const Choice = require('./Choice');
 
 // create associations 
+User.hasMany(Question, {
+    foreignKey: 'user_id'
+});
+
+Question.hasMany(User, {
+    foreignKey: 'question_id'
+})
+
+// DOUBlE CHECK THE FOREIGN KEY AND MAKE SURE THEY THE RIGHT ONES 
+User.belongsToMany(Question, {
+    through: Choice,
+    as: 'choice_selected',
+    foreignKey: 'user_id'
+});
+// DOUBlE CHECK THE FOREIGN KEY AND MAKE SURE THEY THE RIGHT ONES 
+Question.belongsToMany(User, {
+    through: Choice,
+    as:'choice_selected',
+    foreignKey: 'question_id'
+});
+
+Choice.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Choice.belongsTo(Question, {
+    foreignKey: 'question_id'
+});
 
 
 
-
-
-module.exports = { User }; 
+module.exports = { User, Question, Choice }; 
