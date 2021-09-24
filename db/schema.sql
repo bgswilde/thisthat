d0896e9dd89ffeab4c69b5463d90d31daa0dc474
Brian <bgswilde@gmail.com>
2021-09-24 00:36:22 -0500

    seeds and schema for testing

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,32 @@
 DROP DATABASE IF EXISTS this_that_db;
 -- create new database
 CREATE DATABASE this_that_db;
+
+-- code below for testing only, delete on production
+USE this_that_db;
+
+DROP TABLE IF EXISTS choice;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS question;
+
+CREATE TABLE question (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    this_true VARCHAR(50) NOT NULL,
+    that_false VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE user (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE choice (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    q_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    choice BOOLEAN NOT NULL,
+    CONSTRAINT fk_q_id FOREIGN KEY (q_id) REFERENCES question(id) ON DELETE CASCADE,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
