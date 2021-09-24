f801942b78e4c7f3aba07616d4d62371ae903b50
Terror666 <ckuthulu@gmail.com>
2021-09-24 10:28:53 -0500

    clean up some code

--- controllers/api/question-routes.js
+++ controllers/api/question-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { User, Question, Choice } = require('../../models');
 
-// get all users 
+// get all questions 
 router.get('/', (req,res) => {
     Question.findAll({
         order: [['created_at', 'DESC']],
@@ -29,7 +29,7 @@ router.get('/', (req,res) => {
     })
 });
 
-// get single user
+// get single question
 router.get('/:id', (req,res) => {
     Question.findOne({
         where: {
@@ -43,62 +43,6 @@ router.get('/:id', (req,res) => {
         //         model: Comment
         //     }
         // ]
-    })
-    .then(dbUserData => {
-        if(!dbUserData) {
-            res.status(404).json({ message: 'no User with that ID found'});
-            return;
-        }
-        res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-});
-
-// create a user
-router.post('/', (req,res) => {
-    User.create({
-        username: req.body.username,
-        password: req.body.password
-    })
-    .then(dbUserData=> res.json(dbUserData))
-    .catch(err=> {
-        console.log(err);
-        res.status(500).json(err);
-    })
-});
-
-// update user
-router.put('/:id', (req,res) => {
-    User.update(req.body, {
-        // req.body means to only update what is being passed through
-        individualHooks: true,
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(dbUserData => {
-        if(!dbUserData) {
-            res.status(404).json({ message: 'no User with that ID found'});
-            return;
-        }
-        res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-});
-
-
-// delete a user
-router.delete('/:id', (req,res) => {
-    User.destroy({
-        where: {
-            id: req.params.id
-        }
     })
     .then(dbUserData => {
         if(!dbUserData) {
