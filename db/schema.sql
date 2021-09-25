fa4523b3b56e16c6890a2b59097f1a91d0524ee7
Brian <bgswilde@gmail.com>
2021-09-25 16:25:35 -0500

    added empty questions.handlebars page. structure in place to add content to all handlebars needed

--- db/schema.sql
+++ db/schema.sql
@@ -10,24 +10,24 @@ DROP TABLE IF EXISTS choice;
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS question;
 
-CREATE TABLE question (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    this_true VARCHAR(50) NOT NULL,
-    that_false VARCHAR(50) NOT NULL
-);
+-- CREATE TABLE question (
+--     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     this_true VARCHAR(50) NOT NULL,
+--     that_false VARCHAR(50) NOT NULL
+-- );
 
-CREATE TABLE user (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    password VARCHAR(30) NOT NULL
-);
+-- CREATE TABLE user (
+--     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR() NOT NULL,
+--     password VARCHAR(30) NOT NULL
+-- );
 
-CREATE TABLE choice (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    question_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    choice BOOLEAN NOT NULL,
-    CONSTRAINT fk_q_id FOREIGN KEY (question_id) REFERENCES question(id) ON DELETE CASCADE,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
-);
+-- CREATE TABLE choice (
+--     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     question_id INTEGER NOT NULL,
+--     user_id INTEGER NOT NULL,
+--     choice BOOLEAN NOT NULL,
+--     CONSTRAINT fk_q_id FOREIGN KEY (question_id) REFERENCES question(id) ON DELETE CASCADE,
+--     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+-- );
 
