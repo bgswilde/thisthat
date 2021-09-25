8e5c2275c141d36bb9a5626ec2a1f040d6dd8fac
Brian <bgswilde@gmail.com>
2021-09-25 09:57:02 -0500

    fixed a server connection typo, added basic route to render first handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 // all routes will now how prefix of /api
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req,res) => {
     res.status(404).end();
