d49f2f9f18764d2e1c6f1f7a5343b3137375edb9
Terror666 <ckuthulu@gmail.com>
2021-09-26 01:14:36 -0500

    attempting login feature

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,7 +15,7 @@ router.get('/', (req,res) => {
 });
 
 // get single user
-router.get('/:id', (req,res) => {
+/*router.get('/:id', (req,res) => {
     User.findOne({
         // this makes sure we don't see user password
         attributes: { exclude: ['password']},
@@ -42,7 +42,7 @@ router.get('/:id', (req,res) => {
         console.log(err);
         res.status(500).json(err);
     })
-});
+});*/
 
 // create a user
 router.post('/', (req,res) => {
@@ -50,18 +50,46 @@ router.post('/', (req,res) => {
         username: req.body.username,
         password: req.body.password
     })
-    .then(dbUserData=> /*{
+    .then(dbUserData=> {
         req.session.save(() => {
             req.session.user_id = dbUserData.id;
             req.session.username = dbUserData.username;
             req.session.loggedIn = true;
-        })
-    }*/
-    res.json(dbUserData))
+            
+            res.json(dbUserData);
+        });
+    })
     .catch(err=> {
         console.log(err);
         res.status(500).json(err);
     })
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No username found! Please try again or sign up ;)' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect Password!' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
 });
 
 // update user
@@ -86,7 +114,15 @@ router.put('/:id', (req,res) => {
     })
 });
 
-
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+})
 // delete a user
 router.delete('/:id', (req,res) => {
     User.destroy({
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,8 +2,8 @@ async function loginFormHandler(event) {
     event.preventDefault();
 
     // add #ID or .CLASS when they're created
-    const username = document.querySelector('').value.trim();
-    const password = document.querySelector('').value.trim();
+    const username = document.querySelector('#logUsername').value.trim();
+    const password = document.querySelector('#exampleInputPassword2').value.trim();
     // if there is a users then do fetch call 
     if(username && password) {
         const response = await fetch('/api/users/login', {
@@ -17,11 +17,11 @@ async function loginFormHandler(event) {
         // if promise is successful then
         if(response.ok){
             // right now if user exists then it will display success
-            // document.location.replace('/dashboard');
+            document.location.replace('/dashboard');
             console.log('success');
         }
     }
 }
 
 // add #ID or .CLASS when they're created for login button 
-document.querySelector('').addEventListener('submit', loginFormHandler);
+document.querySelector('#loginBtn').addEventListener('click', loginFormHandler);
