94050fe4f5c56b035718ee71731a61e1c52e0acd
Brian <bgswilde@gmail.com>
2021-09-27 19:53:08 -0500

    js files and notes in place for questions

---
+++ public/javascript/questions.js
@@ -0,0 +1,30 @@
+// handlebars will handle incrementing 
+
+let startingPoint = 0;
+// when startingPoint > 21, message to say you've answered everything
+
+/* 
+
+----initial-----
+
+fetch find one question i
+index value ++
+
+ 
+----next action-----
+
+event listener for each option
+when they select their option,
+create the choice 
+selected style gets added class="selected"
+percentage stat hidden class removed, id "top-stat" or "bottom-stat"
+next button hidden class removed
+
+----final action-----
+
+event listener on next button
+when they select next
+hidden classes applied again
+back to initial function
+
+*/
