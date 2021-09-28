09d6960d85f5aef14e16f2fc1af4130547629297
Terror666 <ckuthulu@gmail.com>
2021-09-27 23:54:18 -0500

    'small edit'

--- controllers/question-routes.js
+++ controllers/question-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Choice, Question, User } = require('../models');
 
-router.get('/', (req, res) => {
+router.get('/questions', (req, res) => {
     res.render('questions', {
         loggedIn: req.session.loggedIn
     });
