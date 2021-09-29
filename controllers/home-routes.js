c935eab3521b539e5355c8b52b9efbe40ed64fed
georgedmendoza <georgedmendoza13@gmail.com>
2021-09-29 13:18:45 -0500

    display one question, but only works with first question in db

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -81,13 +81,13 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
-});
+// router.get('/login', (req, res) => {
+//     if (req.session.loggedIn) {
+//         res.redirect('/');
+//         return;
+//     }
+//     res.render('login');
+// });
 
 router.get('/dashboard', (req, res) => {
     if (req.session.loggedIn) {
